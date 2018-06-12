bee32ed3175bf6faca2e8c0d086008184f3d319b
frooia <frooia77@gmail.com>
2018-06-12 11:14:33 +0800

    M03_Grades and PMR finished. PMR template created.

--- src/M03_Grades.java
+++ src/M03_Grades.java
@@ -1,17 +1,22 @@
 /**
- * The purpose of this program is to 
- * calculate the average of test grades 
- * and total points for each test added.
+ * The purpose of this program is to
+ * accept user-input test grades,
+ * calculate the average of the test grades, 
+ * and calculate the total points for all tests combined.
  * 
  * @author Lydia Chung 
- * @version 07/06/2018
+ * @version 12/06/2018
  */
 import java.util.Scanner;
+import java.util.ArrayList;
 public class M03_Grades
 {
     public static void main(String[ ] args)
     {
-        //makes scanner and scanner variables
+        //the gradeList
+    	ArrayList<Integer> gradeList = new ArrayList<Integer>();
+    	
+    	//makes scanner and scanner variables
     	Scanner in = new Scanner(System.in);
     	System.out.print("What is your name: ");
     	String name = in.next();
@@ -23,25 +28,36 @@ public class M03_Grades
     	int gradeLevel = in.nextInt();
     	
     	//add user test score input
+    	for (int i = 0; i < 100; i++) {
+    		System.out.print("What score do you want to input (type q to display averages): ");
+        	String input = in.next();
+    		if (input.equals("q")) {
+    			break;
+    		}
+    		else {
+    			int testScore = Integer.parseInt(input);
+                gradeList.add(testScore); 
+                System.out.println(gradeList);
+    		}
+    	}
     	
     	//local variables
-    	int[] gradeList = {95, 73, 91, 83, 100, 93, 89, 70, 79};
-    	int numTests = gradeList.length;	//counts the number of tests
+    	int numTests = gradeList.size();	//counts the number of tests
     	int testIndex = 0;	//test #
     	int testGrade = 0;	//individual test grade
     	int totalPoints = 0;//total points for all tests
     	double average = 0;	//average grade
     	
-    	//print beginning header
+    	//beginning header
     	System.out.println(name + "\t" + subject + "\t" + "\tGrade " + gradeLevel);
     	
-    	//actually prints scores, is a for loop
-    	for (int i = 0; i < (numTests); i++) {
+    	//actually prints scores
+    	for (int j = 0; j < (numTests); j++) {
     		testIndex++;
-    		testGrade = gradeList[i];
+    		testGrade = gradeList.get(j);
     		totalPoints += testGrade;
     		average = (double) totalPoints / (double) testIndex; //calculates average of all grades so far
-    		System.out.println("Test #" + testIndex + "\t\tTest grade: " + gradeList[i] + "\t\tTotal points: " + totalPoints + "\tAverage: " + average);
+    		System.out.println("Test #" + testIndex + "\t\tTest grade: " + gradeList.get(j) + "\t\tTotal points: " + totalPoints + "\tAverage: " + average);
     	}
     }//end of main method
 }//end of class
