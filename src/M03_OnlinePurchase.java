9c58d583f5c7b6688919ab78d5b3f0bc2e557f69
Lydia Chung <frooia77@gmail.com>
2018-06-12 04:08:46 -0400

    M03_OnlinePurchase finished

--- src/M03_OnlinePurchase.java
+++ src/M03_OnlinePurchase.java
@@ -23,7 +23,7 @@ public class M03_OnlinePurchase
 		String month = date.substring(0, 2);
 		String day = date.substring(3, 5);
 		String year = date.substring(6);
-		System.out.print("What color of teacups would you like to buy? (red/blue/green/yellow): "); //asks for color of teacups
+		System.out.print("What color of teacups would you like to buy? (red/blue/green/yellow) "); //asks for color of teacups
 		String color = in.next();
 		System.out.print("How many teacups would you like to buy? "); //asks for teacups bought
 		String teacupsString = in.next();
@@ -44,6 +44,18 @@ public class M03_OnlinePurchase
 		String pin = in.next();
 		
 		//print statements
-		
+		System.out.println("******************* Receipt *******************");
+		System.out.println("Order Number: " + debitString.substring(1, 4) + lastName.substring(1,3));
+		System.out.println(month + "-" + day + "-" + year);
+		System.out.println(firstName.substring(0, 1) + ". " + lastName);
+		System.out.println();
+		System.out.println("Teacups bought: " + teacupsInt);
+		System.out.println("Color: " + color);
+		System.out.println("Price of 1 teacup: " + priceDouble);
+		System.out.println("Total cost: " + (teacupsInt * priceDouble));
+		System.out.println("Account: XXXXX-XXX-" + debitString3);
+		System.out.println();
+		System.out.println("Thank you for your purchase. Your order should be delivered to you soon.");
+		System.out.println("***********************************************");
 	}
 }
