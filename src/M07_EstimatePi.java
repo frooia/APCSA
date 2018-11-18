c95c5c7b6efe49aa24e095b740c57835842fc363
frooia <frooia77@gmail.com>
2018-11-17 19:41:00 -0500

    M07_EstimatePI and PMR done

--- src/M07_EstimatePi.java
+++ src/M07_EstimatePi.java
@@ -3,14 +3,57 @@
  * a dart thrown at a circle to calculate pi.
  * 
  * @author Lydia Chung
- * @version 11/08/2018
+ * @version 11/09/2018
  * 
  */
 import java.util.Random;
+import java.util.Scanner;
 public class M07_EstimatePi
 {
+	public static double monteCarlo(long darts) //Monte Carlo method
+	{
+		Random randNumList = new Random();
+		double x, y;
+		double hit = 0.0;
+		for (int i = 0; i < darts; i++) {
+			x = randNumList.nextDouble();
+			y = randNumList.nextDouble();
+			if (Math.pow(x, 2) + Math.pow(y, 2) <= 1) {
+				hit++;
+			}
+		}
+		double pi = 4.0 * hit / darts;
+		return pi;
+	}
+	public static double arrayAverage(double [] array) //averages elements in an array
+	{
+		double sum = 0;
+		for (double element : array) {
+			sum += element;
+		}
+		return sum / array.length;
+	}
 	public static void main(String [] args) 
 	{
+		//get user input
+		Scanner in = new Scanner(System.in);
+		System.out.println("How many trials?");
+		int trials = in.nextInt();
+		System.out.println("How many darts per trial?");
+		long darts = in.nextLong();
 		
+		//for each trial, find pi for the number of darts
+		double [] piValues = new double[trials];
+		for (int i = 0; i < trials; i++) {
+			piValues[i] = monteCarlo(darts);
+			System.out.printf("%-7s%3d%3s%-8f", "Trial [", i + 1, "]: ", piValues[i]); 
+			System.out.println();
+		}
+		
+		//find average pi
+		System.out.println("Average pi value: " + arrayAverage(piValues));
+		
+		//It takes about 100 trials at 10000000 darts per trial for the average pi value to approach 3.141592.
+
 	}
 }
--- src/_Testbed.java
+++ src/_Testbed.java
@@ -1,68 +1,52 @@
 
 /**
- * The purpose of this program is to grade a True-False test.
+ * The purpose of this program is to calculate the hypotenuse
+ * of a triangle and its area. All calculations are done within
+ * methods. An object is instantiated and all methods are invoked
+ * on the object.
  *
- * @author Grace Hopper
- * @version 06/07/17
+ * @author Alan Kay
+ * @version 06/08/17
  */
-import java.util.Scanner;
-import java.io.File;
-import java.io.IOException;
 public class _Testbed
 {
-	// Read in test data from text file.
-    public static String [] readTestData()throws IOException
+    //default constructor
+    public _Testbed()
     {
-        int index = 0;
-        String [] answers = new String[11];
-
-        File fileName = new File("src/TestData1.txt");
-        Scanner inFile = new Scanner(fileName);
-        while (inFile.hasNext())
-        {
-            answers[index] = inFile.next( );
-            index++;
-        }
-        inFile.close();
-        return answers;
     }
 
-    // Grade the test and count the number correct.
-    public static int gradeTest(String[] test, String[] key)
+    //calculate area of a triangle
+    public double calcTriArea(int s1, int s2)
     {
-        int correct = 0;
-
-        for(int n = 1; n < test.length; n++)
-        {
-            if(test[n].equals(key[n]))
-                correct++;
-        }
-        return correct;
-     }
-
-     // Calculate the percent correct.
-     public static double calcPercent(int n)
-     {
-         return (n/10.0) * 100;
-     }
-
-
-     // Print the results.
-     public static void printResults(String studentId, double pct)
-     {
-        System.out.printf("Student Number: %s   Grade: %4.1f%n", studentId, pct);
-     }
-
-    // The main method.
-    public static void main(String[] args) throws IOException
-    {
-        String[] testAnswers = new String[11];
-        String[] answerKey = {"name", "T", "F", "T", "F", "T", "F", "T", "F", "T", "T"};
-
-        testAnswers = readTestData();
-        int numCorrect = gradeTest(testAnswers, answerKey);
-        double percent = calcPercent(numCorrect);
-        printResults(testAnswers[0], percent);
+        return s1 * s2 * 0.5;
     }
-}
+    //calculate the hypotenuse of a right triangle
+    public double calcHypotenuse(int s1, int s2)
+    {
+        return Math.sqrt(Math.pow(s1, 2) + Math.pow(s2, 2));
+    }
 
+    //main method
+    public static void main(String[] args)
+    {
+        //declaration of variables
+        int side1, side2;
+        double triArea, hypotenuse;
+
+        //initialization of variables
+        side1 = 10;
+        side2 = 5;
+        triArea = 0;
+        hypotenuse = 0;
+
+        _Testbed shapes = new _Testbed();
+
+        //call methods
+        triArea = shapes.calcTriArea(side1, side2);
+        hypotenuse = shapes.calcHypotenuse(side1, side2);
+
+        //print results
+        System.out.printf(" Triangle Area = %8.2f%n", triArea);
+        System.out.printf(" Hypotenuse = %8.2f%n", hypotenuse);
+    }//end main method
+}//end class
